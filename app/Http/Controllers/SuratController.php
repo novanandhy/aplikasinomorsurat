838c2a7d8ad523084d7454c40f7b396812664e76
Novan PC <novanandhytrianto@gmail.com>
2020-12-21 15:26:59 +0700

    merapikan controller surat

--- app/Http/Controllers/SuratController.php
+++ app/Http/Controllers/SuratController.php
@@ -5,17 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
+use App\Traits\SuratApi;
+use App\Traits\BagianApi;
 
 use App\Models\Surat;
 use App\Models\Bagian;
 
 class SuratController extends Controller
 {
+	use SuratApi;
+	use BagianApi;
+
 	// menampilkan halaman utama pengguna
 	public function index()
 	{
 		// mengambil data dari table bagian
-    	$bagian = Bagian::all();
+    	$bagian = $this.getAllBagian();
  
     	// mengirim data bagian ke view user_aplikasi
     	return view('user_aplikasi', compact('bagian'));
@@ -24,41 +29,22 @@ class SuratController extends Controller
 	// menyimpan data surat
 	public function store(Request $request)
 	{
-		$message = [
-			'required' => ':attribute wajib diisi',
-			'min' => ':attribute diisi minimal :min karakter',
-			'max' => ':attribute diisi maksimal :max karakter'
-		];
+		// cek validasi form
+		$this->validateForm($request);
+		
+		// memanggil method createSurat
+		$data = $this->createSurat($request);
 
-		$this->validate($request,[
-			'pengirim' => 'required',
-			'tujuanSurat' => 'required|min:7',
-			'tujuanInstansi' => 'required|min:7',
-			'tanggalSurat' => 'required|min:10|max:10',
-			'kodeSurat' => 'required|min:15',
-			'perihalSurat' => 'required|min:7'
-		], $message);
+		// cek apakah data telah ada
+		if($data === null){
 
-		$exist = Surat::where('id_pengirim', $request->get('pengirim'))->where('tujuan_surat', $request->get('tujuanSurat'))->where('tujuan_instansi', $request->get('tujuanInstansi'))->where('tanggal_surat', $request->get('tanggalSurat'))->where('kode_surat', $request->get('kodeSurat'))->where('perihal_surat', $request->get('perihalSurat'))->first();
-
-		if($exist === null){
-			$surat = new Surat([
-				'id_pengirim' => $request->get('pengirim'),
-				'tujuan_surat' => $request->get('tujuanSurat'),
-				'tujuan_instansi' => $request->get('tujuanInstansi'),
-				'tanggal_surat' => $request->get('tanggalSurat'),
-				'kode_surat' => $request->get('kodeSurat'),
-				'perihal_surat' => $request->get('perihalSurat'),
-				'created_at' => Carbon::now()
-			]);
-	
-			$surat->save();
-			
-			$data = Surat::where('tujuan_surat', $surat['tujuan_surat'])->where('id_pengirim', $surat['id_pengirim'])->where('perihal_surat', $surat['perihal_surat'])->first();
-	
-			return view('user_cetaknomor', compact('data'));
-		}else{
+			//jika tidak terkirim data, maka akan kembali ke halaman awal aplikasi
 			return redirect()->route('aplikasi.index')->with('error','data surat telah ada');
+			
+		}else{
+
+			//jika data terkirim, maka akan masuk ke halaman tampilan data nomor surat
+			return view('user_cetaknomor', compact('data'));
 		}
 	}
 
@@ -69,7 +55,50 @@ class SuratController extends Controller
 	}
 
 	//  memperbarui alamat
-	public function update(Request $request, Surat $surat)
+	public function update(Request $request, $id)
+	{
+		// cek validasi form
+		$this->validateForm($request);
+
+		// memanggil method updateSurat
+		$this.updateSurat($request, $id);
+
+		return redirect()->route('aplikasi.admin')->with('success','data surat berhasil diperbarui');
+	}
+
+	// menghapus data
+	public function destroy($id)
+	{
+		//memanggil method deleteSurat
+		$this.deleteSurat($id);
+
+		return redirect()->route('aplikasi.admin')->with('success','data surat berhasil dihapus');
+	}
+
+	// menampilkan halaman utama admin
+	public function admin()
+	{
+		// mengambil data dari table surat
+    	$surat = $this->getAllSurat();
+ 
+    	// mengirim data bagian ke view admin_daftarsurat
+    	return view('admin_daftarsurat', compact('surat'));
+	}
+	
+	// menampilkan autocomplete tujuan surat
+	public function autocomplete_tujuan(Request $request)
+	{
+		return $this->tujuanSurat($request);
+	}
+
+	// menampilkan autocomplete tujuan instansi surat
+	public function autocomplete_instansi(Request $request)
+	{
+		return $this->instansiSurat($request);
+	}
+
+	//melakukan validasi form
+	public function validateForm(Request $request)
 	{
 		$message = [
 			'required' => ':attribute wajib diisi',
@@ -85,62 +114,5 @@ class SuratController extends Controller
 			'kodeSurat' => 'required|min:15',
 			'perihalSurat' => 'required|min:7'
 		], $message);
-
-		$surat->update(['id_pengirim' => $request->get('pengirim'),
-		'tujuan_surat' => $request->get('tujuanSurat'),
-		'tujuan_instansi' => $request->get('tujuanInstansi'),
-		'tanggal_surat' => $request->get('tanggalSurat'),
-		'kode_surat' => $request->get('kodeSurat'),
-		'perihal_surat' => $request->get('perihalSurat'),
-		'updated_at' => Carbon::now()]);
-
-		return redirect()->route('aplikasi.admin')->with('success','data surat berhasil diperbarui');
 	}
-
-	// menghapus data
-	public function destroy(Surat $surat)
-	{
-		$surat -> delete();
-
-		return redirect()->route('aplikasi.admin')->with('success','data surat berhasil dihapus');
-	}
-
-	// menampilkan halaman utama admin
-	public function admin()
-	{
-		// mengambil data dari table surat
-    	$surat = Surat::all();
- 
-    	// mengirim data bagian ke view admin_daftarsurat
-    	return view('admin_daftarsurat', compact('surat'));
-	}
-	
-	// menampilkan autocomplete tujuan surat
-	public function autocomplete_tujuan(Request $request)
-	{
-		$search = $request->get('query');
-		$result = Surat::select('tujuan_surat')->where('tujuan_surat', 'LIKE','%'.$search.'%')->get();
-
-		$data = array();
-		foreach ($result as $result) {
-			$data[]=$result->tujuan_surat;
-		}
-
-		return response()->json($data);
-	}
-
-	// menampilkan autocomplete tujuan instansi surat
-	public function autocomplete_instansi(Request $request)
-	{
-		$search = $request->get('query');
-		$result = Surat::select('tujuan_instansi')->where('tujuan_instansi', 'LIKE','%'.$search.'%')->get();
-
-		$data = array();
-		foreach ($result as $result) {
-			$data[]=$result->tujuan_instansi;
-		}
-
-		return response()->json($data);
-	}
-
 }
